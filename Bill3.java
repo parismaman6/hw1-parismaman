db45f0fdfcd88d10872d90cacf8ec2b3f609896b
parismaman6 <paris.maman@post.runi.ac.il>
2025-11-06 12:01:07 +0200

    Completed assignment

--- Bill3.java
+++ Bill3.java
@@ -1,14 +1,11 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
 		int billAmount = Integer.parseInt(args[3]);
 		double splitAmount = billAmount / 3.0;
 		splitAmount= Math.ceil(splitAmount);
-		System.out.println("Dear " + name3 + ", " + name2 + " and "  + name1 + " : pay " + splitAmount + "each.");
-	    // Replace this comment with the rest of your code   
+		System.out.println("Dear " + name3 + ", " + name2 + ", and "  + name1 + " : pay " + splitAmount + " Shekels each.");
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,11 +1,11 @@
-// Computes the future value of a saving investment.
+
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt (args[0]);
-		double rate = Double.parseDouble(args[1]);
+		double rate = Double.parseDouble(args[1]) ;
 		int n = Integer.parseInt (args[2]);
 		double futureValue= currentValue * Math.pow((1 + rate), n);
 		futureValue= futureValue/ 100.0;
-		System.out.println("After " + n + " years, a " + currentValue + "$ saved at " + rate + "% will yeild " + (int) futureValue + "$.");
+		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
 	}
 }
